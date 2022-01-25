05ff9b48ba9426f5f98bbae9635d4cf97ce0d945
tallner <ctallner@hotmail.com>
2022-01-25 23:30:39 +0100

    Defined callbacks
    Rewrote and tested with defined callback functions

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # javascript1-assignment3-tallner
 use flickr api to generate random pictures on a website
+
+
+
+Q
+how to test for errors in the api, i always get 200
---
+++ js/flickr_script.js
@@ -0,0 +1,102 @@
+//My KEY at flickr
+const KEY = `052a884f5955c2c6e26e1c31ad61e000`;
+
+//Test searchfield, replaced by input later
+let searchText = `dog`;
+
+//Use pagination, 1 picture, 1 page
+const url = `https://www.flickr.com/services/rest/?api_key=${KEY}&method=flickr.photos.search&text=${searchText}&format=json&nojsoncallback=1&per_page=1&page=1`;
+
+//fetch returnerar promise
+// const flickrPromise = fetch(url);
+// console.log(flickrPromise);
+
+//när promise fulfilled anropas callback inom then-metoden
+// const jsonPromise = flickrPromise.then(
+//     function(response){
+//         console.log(response);
+//         if(response.status>=200 && response.status<300){
+//             return response.json();
+//         }
+//         else{
+//             throw 'Something went wrong. :(';
+//         }
+        
+//     }
+// );
+//console.log(jsonPromise);
+
+// const objPromise = jsonPromise.then(
+//     function(response){
+//         console.log(response);
+//     }
+// );
+// console.log(objPromise);
+
+// const catchPromise = flickrPromise.catch(
+//     function(error){
+//         console.log(error);
+//     }
+// );
+// console.log(catchPromise);
+
+
+
+//samma men med definierade callbackfunktioner
+//fetch(url).then(getImageUrl).then();
+
+// function responseCallback(response){
+//     console.log(response);
+//     return response.json();
+// }
+
+// function showImgCallback(data){
+//     const img = document.createElement('img');
+//     img.src = data.message;
+//     document.body.append(img);
+// }
+
+fetch(url).then(responseFunction).then(dataFunction).catch(errorFunction);
+
+function responseFunction(response){
+    console.log(response);
+        if(response.status>=200 && response.status<300){
+            return response.json();
+        }
+        else{
+            throw 'Something went wrong. :(';
+        }
+}
+
+function dataFunction(data){
+    console.log(data);
+        //Vi hämtar första bilden
+        getImageUrl(data.photos.photo[0]);
+}
+
+function errorFunction(error){
+    console.log(error);
+}
+
+//här ska vi pussla ihop bild-urlen
+function getImageUrl(photoObject){
+    let photo = photoObject;
+    let size = 'm';
+
+    let imgUrl = `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_${size}.jpg`;
+
+    // console.log(imgUrl);
+    displayImg(imgUrl);
+}
+
+//för att visa bilden
+function displayImg(url){
+    let img = document.createElement('img');
+    img.src = url;
+
+    document.body.appendChild(img);
+}
+
+function errorHandling(error){
+    console.log(error);
+}
